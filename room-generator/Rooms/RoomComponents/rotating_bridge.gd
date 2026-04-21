d5a6fdb7175ee4a21de008049c92f86331b6c5a8
martin1890 <36193985+martin1890@users.noreply.github.com>
2026-04-21 12:22:42 +0200

    fixes #14
    added a character and made it able to look around with the cursor

---
+++ room-generator/Rooms/RoomComponents/rotating_bridge.gd
@@ -0,0 +1,23 @@
+extends StaticBody3D
+
+@export var target_angle_deg: float = 90.0
+@export var rotation_speed: float = 5.0
+@export var stop_threshold_deg: float = 0.5
+
+var should_change: bool = false
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("rotate_bridge"):
+		should_change = true
+
+	if should_change:
+		change_position(delta)
+
+func change_position(delta: float) -> void:
+	var target_y := deg_to_rad(target_angle_deg)
+	rotation.y = lerp_angle(rotation.y, target_y, rotation_speed * delta)
+
+	if abs(angle_difference(rotation.y, target_y)) < deg_to_rad(stop_threshold_deg):
+		rotation.y = target_y
+		should_change = false
